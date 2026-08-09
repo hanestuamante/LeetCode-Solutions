ec826950c51a97958280677df79430a2647842f4
Vo Quoc Dai <voquocdai.work@icloud.com>
2026-08-09 13:09:53 +0700

    Time: 3 ms (11.65%), Space: 23.7 MB (5.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,16 +4,16 @@ public:
         vector<int> arr;
         unordered_set<int> se; 
 
-        for(int i = 0; i < nums.size(); i++) {
+        for(int i = 0; i < nums.size(); i++){
             int temp = nums[i];
 
-            if(se.find(temp) == se.end()) { 
+            if(se.find(temp) == se.end()){ // Tim xem temp co trong set khong, se.end() tra ve khi khong tim thay
                 se.insert(temp);
                 arr.push_back(temp);
             }
         }
 
-        nums = arr; 
+        nums = arr; // do trinh cham co luu y, nen em se truyen tham chieu theo vector<int>& nums
         return nums.size();
     }
 };
