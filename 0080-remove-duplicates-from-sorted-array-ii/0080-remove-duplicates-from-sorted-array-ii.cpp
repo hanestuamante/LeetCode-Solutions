761eac9af438ce0e602c32850a1682ac99d16bca
Vo Quoc Dai <voquocdai.work@icloud.com>
2026-08-09 13:33:16 +0700

    Time: 4 ms (84.18%), Space: 19.5 MB (60.86%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,20 +1,17 @@
-
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> arr;
-        unordered_map<int, int> freq; // Key: số trong nums, Value: số lần xuất hiện
+        if(nums.empty()) return 0;
+        int k = 0;
 
-        for (int temp : nums) {
-            // Kiểm tra điều kiện: Nếu key này chưa xuất hiện quá 2 lần
-            if (freq[temp] < 2) {
-                arr.push_back(temp); // Thêm vào mảng kết quả
-                freq[temp]++;        // Đánh dấu tăng số lần xuất hiện
+        for(int i = 0; i < nums.size(); i++){
+            if(k < 2 || nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
             }
-            // Nếu freq[temp] == 2 thì bỏ qua, không push_back nữa
         }
 
-        nums = arr;          // Ghi đè lại mảng gốc
-        return nums.size();  // Trả về độ dài mảng thỏa mãn
+        return k;
+        
     }
 };
